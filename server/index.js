cf94f801c975b14df2c9a7351afd11aa7e58505b
tmneth <80415416+tmneth@users.noreply.github.com>
2022-04-24 10:07:56 +0300

    Configure routes

--- server/index.js
+++ server/index.js
@@ -1,4 +1,20 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv'
+import postsRoutes from './routes/posts.js'
+
+const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postsRoutes);
+
+mongoose.connect(process.env.CONNECTION_URL).then(() => app.listen(process.env.PORT, () => console.log(`Server Running on Port: http://localhost:${process.env.PORT}`)))
+
+

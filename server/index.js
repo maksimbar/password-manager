dbd52a83cf83439ac29a0800284ca495837cc331
tmneth <80415416+tmneth@users.noreply.github.com>
2022-05-02 19:16:12 +0300

    Bug fixes

--- server/index.js
+++ server/index.js
@@ -3,11 +3,18 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv'
+import path from 'path'
 import postsRoutes from './routes/posts.js'
 import userRoutes from "./routes/users.js";
+
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 5000
 
 const app = express();
+
+app.use(express.static(path.join(__dirname,'/client/build')))
+
 
 dotenv.config();
 
@@ -18,16 +25,16 @@ app.use(cors());
 app.use('/posts', postsRoutes);
 app.use('/user', userRoutes);
 
-app.get("*", function(_,res) {
-    res.sendFile(
-        path.join(__dirname, "./client/build/index.html"),
-        function(err) {
-            if(err) {
-                res.status(500).send(err)
-            }
-        }
-    )
-})
+// app.get("*", function(_,res) {
+//     res.sendFile(
+//         path.join(__dirname, "./client/build/index.html"),
+//         function(err) {
+//             if(err) {
+//                 res.status(500).send(err)
+//             }
+//         }
+//     )
+// })
 
 mongoose.connect(process.env.CONNECTION_URL).then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
 

0964a76e2d1db0dc9c150cd7156f4c8a98a430ed
tmneth <80415416+tmneth@users.noreply.github.com>
2022-05-02 12:25:45 +0300

    Wrap app in Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
+import { reducers } from './reducers';
+import App from './App';
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store}>
     <App />
+  </Provider>,
 );
-

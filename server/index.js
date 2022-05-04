4db26cacc89d66fc52f11bc933dea364a2ae1c15
tmneth <80415416+tmneth@users.noreply.github.com>
2022-05-04 12:56:53 +0300

    Prepare project for deployment

--- server/index.js
+++ server/index.js
@@ -3,18 +3,13 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv'
-import path from 'path'
 import postsRoutes from './routes/posts.js'
 import userRoutes from "./routes/users.js";
 
-const __dirname = path.resolve();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 const app = express();
-
-app.use(express.static(path.join(__dirname,'/client/build')))
-
 
 dotenv.config();
 
@@ -25,16 +20,6 @@ app.use(cors());
 app.use('/posts', postsRoutes);
 app.use('/user', userRoutes);
 
-// app.get("*", function(_,res) {
-//     res.sendFile(
-//         path.join(__dirname, "./client/build/index.html"),
-//         function(err) {
-//             if(err) {
-//                 res.status(500).send(err)
-//             }
-//         }
-//     )
-// })
 
 mongoose.connect(process.env.CONNECTION_URL).then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
 
